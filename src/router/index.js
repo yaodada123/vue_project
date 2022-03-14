761f024fc1735960b6d3d002deb620694145e5f0
yhc <2580577036@qq.com>
2022-03-15 00:37:55 +0800

    the first commit contain frame

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Home = () => import("../views/home/Home.vue")
+const Category = () => import("../views/category/Category.vue")
+const Cart = () => import('../views/cart/Cart.vue')
+const Profile = () => import('../views/profile/Profile')
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
+      component: Home,
+    },
+    {
+      path: '/category',
+      component: Category,
+    },
+    {
+      path: '/profile',
+      component: Profile,
+    }
+  ],
+  mode: 'history'
+})
